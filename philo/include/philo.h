c5bee2ca063727519c241c5a9fc31115d82801ac
Benjamin Plante <benplante99@gmail.com>
2023-12-23 02:01:20 -0500

    timestamps

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,13 +6,18 @@
 /*   By: bplante/Walord <benplante99@gmail.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/23 00:58:01 by bplante/Wal       #+#    #+#             */
-/*   Updated: 2023/12/23 00:58:25 by bplante/Wal      ###   ########.fr       */
+/*   Updated: 2023/12/23 01:55:59 by bplante/Wal      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
+# include <stdio.h>
+# include <sys/time.h>
+# include <unistd.h>
 
+void		print_state(int philo_num, char *message, long long start_time);
+long long	timestamp(void);
 
 #endif
--- philo/src/print_state.c
+++ philo/src/print_state.c
@@ -1,21 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   print_state.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bplante/Walord <benplante99@gmail.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/23 00:58:03 by bplante/Wal       #+#    #+#             */
-/*   Updated: 2023/12/23 02:01:03 by bplante/Wal      ###   ########.fr       */
+/*   Created: 2023/12/23 01:12:40 by bplante/Wal       #+#    #+#             */
+/*   Updated: 2023/12/23 02:00:49 by bplante/Wal      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int	main(int argc, char **argv)
+long long	timestamp(void)
 {
-	long long start_time;
-	start_time = timestamp();
-	
-	print_state(1, "test", start_time);
+	struct timeval	t;
+
+	gettimeofday(&t, NULL);
+	return ((t.tv_sec * 1000) + (t.tv_usec / 1000));
+}
+
+void	print_state(int philo_num, char *message, long long start_time)
+{
+	long long new_time;
+
+	new_time = timestamp() - start_time;
+	printf("%lld : %i %s\n", new_time, philo_num, message);
 }
