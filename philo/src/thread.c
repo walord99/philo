7e7e02a7a7c58cc585b45a3e0058afdca43c3b2a
Walord <benplante99@gmail.com>
2024-01-11 01:49:11 -0500

    fixed issue where thread wouldnt lock right fork and thread hanging and wrong print order

--- philo/src/thread.c
+++ philo/src/thread.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   thread.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bplante/Walord <benplante99@gmail.com>     +#+  +:+       +#+        */
+/*   By: bplante <bplante@student.42quebec.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 15:27:18 by bplante/Wal       #+#    #+#             */
-/*   Updated: 2024/01/08 15:39:32 by bplante/Wal      ###   ########.fr       */
+/*   Updated: 2024/01/11 01:40:39 by bplante          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,11 @@
 void	log_state(struct s_thread *th, char *msg, int sleep_ms)
 {
 	if (!th->info->is_dead)
+	{
 		print_state(th->pos, msg, th->info->start_time);
-	if (sleep_ms)
-		usleep(sleep_ms * 1000);
+		if (sleep_ms)
+			usleep(sleep_ms * 1000);
+	}
 }
 
 void	*philo_thread(void *arg_struct)
@@ -27,14 +29,17 @@ void	*philo_thread(void *arg_struct)
 	th = (struct s_thread *)arg_struct;
 	while (th->eat_count != th->info->eat_count_max && !th->info->is_dead)
 	{
-		pthread_mutex_lock(th->left_fork);
+		if (!th->info->is_dead)
+			pthread_mutex_lock(th->left_fork);
 		log_state(th, "has taken a fork", 0);
+		if (!th->info->is_dead)
+			pthread_mutex_lock(th->right_fork);
 		log_state(th, "has taken a fork", 0);
 		pthread_mutex_lock(&th->eat_lock);
 		th->eat_count++;
 		th->last_time_eat = timestamp();
-		pthread_mutex_unlock(&th->eat_lock);
 		log_state(th, "is eating", th->info->time_eat_ms);
+		pthread_mutex_unlock(&th->eat_lock);
 		pthread_mutex_unlock(th->right_fork);
 		pthread_mutex_unlock(th->left_fork);
 		log_state(th, "is sleeping", th->info->time_sleep_ms);
